1a9081ae2a481c62361e11f4f82fd317072f793f
Rayhan Khan <rayhankhan1226@gmail.com>
2025-06-22 15:54:20 +0600

    import necessary libray

--- Server/myapp/views.py
+++ Server/myapp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import numpy as np
+import tensorflow as tf
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser
+from PIL import Image
+from decouple import config  
+from rest_framework import status
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+import os
+import uuid
+import requests
+import cv2
+from dotenv import load_dotenv
+from ultralytics import YOLO
+import base64
+#from .generative import generate_road_damage_report, generate_stream
+from django.http import StreamingHttpResponse
 
-# Create your views here.
+
+load_dotenv()
+model_path = os.getenv("MODEL_PATH")
+model = YOLO(model_path)
--- Server/server/server/settings.py
+++ Server/server/server/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders',
+    'rest_framework',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
