3782a2033ec3dba1e8dad020ec959c425b85890e
Rayhan Khan <rayhankhan1226@gmail.com>
2025-06-22 17:06:03 +0600

    updated segmentation analysis base code

---
+++ Server/myapp/segmentation.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+class SegmentationAnalyzer:
+    def __init__(self, masks, classes, class_names, scale_bar_microns=0.5, scale_bar_pixels=72):
+        self.masks = masks
+        self.classes = classes
+        self.class_names = class_names
+        self.scale_bar_microns = scale_bar_microns
+        self.scale_bar_pixels = scale_bar_pixels
+        self.micron_per_pixel = scale_bar_microns / scale_bar_pixels
+
+    def _polygon_area(self, polygon):
+        x = polygon[:, 0]
+        y = polygon[:, 1]
+        return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
+
+    def _pixel_to_micron2(self, pixel_area):
+        return pixel_area * (self.micron_per_pixel ** 2)
+
+    def analyze(self):
+        results = {}
+
+        for seg, cls_idx in zip(self.masks, self.classes):
+            label = self.class_names[cls_idx]
+            polygon = np.array(seg, dtype=np.float32)
+            area_px = self._polygon_area(polygon)
+            area_um2 = self._pixel_to_micron2(area_px)
+
+            if label not in results:
+                results[label] = {"area_um2": 0.0, "count": 0}
+
+            results[label]["area_um2"] += area_um2
+            results[label]["count"] += 1
+
+        for label in results:
+            results[label]["area_um2"] = round(results[label]["area_um2"], 2)
+
+        return results, round(self.micron_per_pixel, 6)
